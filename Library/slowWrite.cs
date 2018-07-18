05c5e02adf22a69ebf97e017d9909052631c539c
Rafael-Romeu <rafaelromeufurg@gmail.com>
2018-07-18 20:06:00 -0300

    Adiciona pastas: 'dll Files', Library & PatternWarriorsCodes com seus respectivos arquivos

---
+++ Library/slowWrite.cs
@@ -0,0 +1,38 @@
+﻿/**
+ * ..:: Library for Prints ::..
+ * 
+ * @author Rafael Romeu
+ * @author Gabriel Moraes 
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Library
+{
+	public static class library
+	{
+		/**
+        * slowWrite
+        *
+        * Metodo para a escrever na tela letra a letra(Estilo Maquina de Escrever).
+        * 
+        * @param string message
+        * @param int delay
+        * @return void
+        * 
+        */
+		public static void slowWrite(string message, int delay)
+		{
+			foreach (char letter in message)
+			{
+				Console.Write(letter);
+				System.Threading.Thread.Sleep(delay);
+			}
+			Console.WriteLine("");
+		}
+	}
+}
+

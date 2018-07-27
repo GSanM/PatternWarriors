a8ce6d08e9e19c85bd699c7da377d4919df823f1
Gabriel <gsan.moraes@gmail.com>
2018-07-27 14:43:05 -0300

    XML Saves Added

--- PatternWarriorsCodes/PatternWarriorsCodes/Menu.cs
+++ PatternWarriorsCodes/PatternWarriorsCodes/Menu.cs
@@ -3,6 +3,8 @@ using System;
 using System.IO;
 using System.Text;
 using System.Collections.Generic;
+using System.Xml;
+using System.Threading;
 
 static class Constants
 {
@@ -15,9 +17,110 @@ namespace Menu
 {
 	public class Conta
 	{
-		private string Username { get; set; }
+		public string Username { get; set; }
 		private string Senha { get; set; }
 
+        //Cria Arquivo XML com header
+        private void CriaXml()
+		{
+			XmlDocument doc = new XmlDocument();
+
+			XmlDeclaration xmlDeclaration = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+            XmlElement root = doc.DocumentElement;
+			doc.InsertBefore(xmlDeclaration, root);
+
+			XmlElement elementUsers = doc.CreateElement(string.Empty, "Users", string.Empty);
+            doc.AppendChild(elementUsers);
+
+			doc.Save("database.xml");
+            
+		}
+
+        //Insere conta no arquivo XML
+		private void InsereContaXml(string user, string pass)
+		{
+			XmlDocument doc = new XmlDocument();
+			doc.Load("database.xml");
+
+			XmlNode NodeUsers = doc.SelectSingleNode("Users");
+
+            XmlElement elementUser = doc.CreateElement(string.Empty, "User", string.Empty);
+            NodeUsers.AppendChild(elementUser);
+
+            XmlElement elementUsername = doc.CreateElement(string.Empty, "username", string.Empty);
+            XmlText username = doc.CreateTextNode(user);
+            elementUsername.AppendChild(username);
+            elementUser.AppendChild(elementUsername);
+
+            XmlElement elementPass = doc.CreateElement(string.Empty, "password", string.Empty);
+            XmlText passwd = doc.CreateTextNode(pass);
+            elementPass.AppendChild(passwd);
+            elementUser.AppendChild(elementPass);
+
+            XmlElement elementSave = doc.CreateElement(string.Empty, "save", string.Empty);
+            elementUser.AppendChild(elementSave);
+
+			doc.Save("database.xml");
+		}
+
+        //Salva no arquivo XML o progresso do usuario
+        public void Salvar(string username, string fase)
+		{
+			XmlDocument doc = new XmlDocument();
+			doc.Load("database.xml");
+            
+			XmlNodeList aNodes = doc.SelectNodes("/Users/User");
+			foreach(XmlNode node in aNodes)
+			{
+				XmlNode NodeUsername = node.SelectSingleNode("username");
+				if (NodeUsername.InnerText == username)
+				{
+					XmlNode NodeSave = node.SelectSingleNode("save");
+                    NodeSave.InnerText = fase;
+				}
+			}
+                       
+			doc.Save("database.xml");
+		}
+
+        //Procura o save do usuario no arquivo XML
+		public string Load(string username, string passw)
+		{
+			bool found = false;
+			string answer = "noSave";
+			XmlDocument doc = new XmlDocument();
+            doc.Load("database.xml");
+
+            XmlNodeList aNodes = doc.SelectNodes("/Users/User");
+            foreach (XmlNode node in aNodes)
+            {
+                XmlNode NodeUsername = node.SelectSingleNode("username");
+                if (NodeUsername.InnerText == username)
+                {
+					XmlNode NodePassw = node.SelectSingleNode("password");
+					if (NodePassw.InnerText == passw)
+					{
+						XmlNode NodeFase = node.SelectSingleNode("save");
+						answer = NodeFase.InnerText;
+						found = true;
+						break;
+					}
+                }
+            }
+			Console.WriteLine("");
+			if (found == true)
+			{
+				library.slowWrite("Loading...", Constants.TEXT_SPEED3, true);
+			}
+			else if(found == false)
+			{
+				library.slowWrite("Incorrect username or password!", Constants.TEXT_SPEED1, true);
+			}
+			Thread.Sleep(2000);
+			return answer;
+		}
+
+        //Cria uma nova conta
         public int CriaConta()
 		{
 			library.slowWrite("Welcome to Pattern Warriors! Let's create an account for you.", Constants.TEXT_SPEED2, true);
@@ -27,6 +130,7 @@ namespace Menu
 			while (true)
             {
                 var key = System.Console.ReadKey(true);
+				Console.Write("*");
                 if (key.Key == ConsoleKey.Enter)
                     break;
 				if (key.Key == ConsoleKey.Backspace)
@@ -35,8 +139,41 @@ namespace Menu
                     Senha += key.KeyChar;
             }
 
-            //Salvar no arquivo
+			//Salvar no arquivo
+			if (!File.Exists("database.xml"))
+			    this.CriaXml();
+			this.InsereContaXml(Username, Senha);
+           
+			return 1;
+		}
 
+        //Faz login em uma conta ja existente
+        public int Login()
+		{
+			string save = "noSave";
+
+            while (save == "noSave")
+			{
+				Console.Clear();
+				Senha = "";
+				library.slowWrite("Username: ", Constants.TEXT_SPEED2, false);
+                Username = Console.ReadLine();
+                library.slowWrite("Password: ", Constants.TEXT_SPEED2, false);
+                while (true)
+                {
+                    var key = System.Console.ReadKey(true);
+					Console.Write("*");
+                    if (key.Key == ConsoleKey.Enter)
+                        break;
+                    if (key.Key == ConsoleKey.Backspace)
+                        Senha.Remove(Senha.Length - 1);
+                    else
+                        Senha += key.KeyChar;
+                }
+                
+                save = this.Load(Username, Senha);
+			}
+            
 			return 1;
 		}
 
@@ -157,6 +294,16 @@ namespace Menu
 			//Convert.ToInt32(Console.ReadLine());
 		}
 
+		public void ImprimeFala(int fala, string username)
+        {
+            switch (fala)
+            {
+                case 1:
+                    library.slowWrite(username += " was searching for knowledge, then he decided to leave in his journey to find the Pattern Scrolls. Each scroll hold the secret of a certain pattern. To reach them, it is necessary to defeat the monsters that keep them.", Constants.TEXT_SPEED2, true);
+                    break;
+            }
+        }
+
         public void SetChosed()
 		{
 
@@ -167,9 +314,14 @@ namespace Menu
 			{
 				case 1:
 					Conta nova = new Conta();
+					MenuPrincipal menuPrincipal = Instance;
 					nova.CriaConta();
+					Console.Clear();
+					menuPrincipal.ImprimeFala(1, nova.Username);
 					break;
 				case 2:
+					Conta nova2 = new Conta();
+					nova2.Login();
 					break;
 				case 3:
 					Console.Clear();
@@ -189,7 +341,8 @@ namespace Menu
             //Teste de Save
 			Save memoria = new Save();
 			Caretaker armazenador = new Caretaker(memoria);
-
+            
+			/*
 			memoria.SetState("Estado 1");
 			armazenador.SaveState();
 			Console.WriteLine(memoria.GetState());
@@ -199,6 +352,7 @@ namespace Menu
 
 			armazenador.RestoreState();
 			Console.WriteLine(memoria.GetState());
+            */
 
 			return 0;
 		}
